23dcc272180960133642bc5279bd844c390eebdf
leckman-tech <leckman@seeforever.org>
2026-03-25 12:39:45 -0400

    cleanup

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -20,6 +20,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 0.8
     },
     {
+      url: `${SITE_URL}/lobby-day`,
+      lastModified: now,
+      changeFrequency: "weekly",
+      priority: 0.78
+    },
+    {
       url: `${SITE_URL}/speakers`,
       lastModified: now,
       changeFrequency: "weekly",
